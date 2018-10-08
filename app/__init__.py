49b13d67e1e70e41b312ca89aa775043696ff999
IgoPereiraBarros <igorestacioceut@gmail.com>
2018-10-07 23:04:43 -0300

    Adicionando bottle_session, para por tempo de duracao na aba login

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import bottle_session
 from bottle import Bottle
 from bottle import TEMPLATE_PATH
 from bottle.ext import sqlalchemy
@@ -20,7 +21,10 @@ plugin = sqlalchemy.Plugin(
     use_kwargs=False
 )
 
+plugin_session = bottle_session.SessionPlugin(cookie_lifetime=120)
+
 app.install(plugin)
+app.install(plugin_session)
 
 from app.controllers import default
 from app.models import tables
